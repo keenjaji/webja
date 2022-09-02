10dad5f0a630d60f44d6e0c05a2d91fe9e9b3383
keenjaji <110646868+keenjaji@users.noreply.github.com>
2022-09-02 11:25:39 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,4 @@
 import React from 'react'
-import MsgBox from "./class-props"
-function App() {
-  return (
-    <MsgBox
-    text="React & React Native"
-    color="blue"bgColor="#ccc"
-    fontSize="18pt"border="solid 1px black"/>
-    ) 
-  }export default App
+import {EventData1}  from './event-data'
+export default 
+function App() {return <EventData1/> }
